6afcdb94c5054ef8b108860827e76bae02308985
varshini00111 <varshinigummadi2004@gmail.com>
2026-05-02 03:37:21 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,1 +1,26 @@
+from flask import Flask, render_template, request, redirect
 
+app = Flask(__name__)
+
+articles = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', articles=articles)
+
+@app.route('/create', methods=['POST'])
+def create():
+    title = request.form['title']
+    author = request.form['author']
+    body = request.form['body']
+
+    articles.append({
+        'title': title,
+        'author': author,
+        'body': body
+    })
+
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run()
