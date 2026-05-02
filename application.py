cc2b72a06f239e557ed012614fca16273968b628
varshini00111 <varshinigummadi2004@gmail.com>
2026-05-02 18:44:30 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,35 +1,6 @@
-import logging
+from app import create_app
 
-logging.basicConfig(level=logging.INFO)
-from flask import Flask, render_template, request, redirect
-import logging
-
-app = Flask(__name__)
-
-logging.basicConfig(level=logging.INFO)
-
-articles = []
-
-@app.route('/')
-def index():
-    return render_template('index.html', articles=articles)
-
-@app.route('/create', methods=['POST'])
-def create():
-    title = request.form['title']
-    author = request.form['author']
-    body = request.form['body']
-
-    articles.append({
-        'title': title,
-        'author': author,
-        'body': body
-    })
-
-    logging.info("admin logged in successfully")
-    return redirect('/')
-
-logging.warning("Invalid login attempt")
+app = create_app()
 
 if __name__ == '__main__':
     app.run()
