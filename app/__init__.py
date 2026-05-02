8dad365e5c4d6817ebf3da95cb68cb3c1bfa2064
varshini00111 <varshinigummadi2004@gmail.com>
2026-05-02 19:13:50 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,13 @@
+from flask import Flask
+import logging
 
+def create_app():
+    app = Flask(__name__)
+
+    # Logging setup
+    logging.basicConfig(level=logging.INFO)
+
+    from .views import main
+    app.register_blueprint(main)
+
+    return app
